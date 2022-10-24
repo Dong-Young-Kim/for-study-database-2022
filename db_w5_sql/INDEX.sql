e245e49ce2e747e322aa3457e821d5f2cc6cb143
DongYoung <bigdykim@gmail.com>
2022-10-25 01:47:16 +0900

    week9 class solving

--- db_w5_sql/INDEX.sql
+++ db_w5_sql/INDEX.sql
@@ -1,6 +1,108 @@
 CREATE SCHEMA IF NOT EXISTS `ex_db1` DEFAULT CHARACTER SET utf8 ;
 USE `ex_db1` ;
 
+-- t1, t2, t3 생성
+create table t1(id int, name varchar(20));
+create table t2(id int, title varchar(20));
+
+insert into t1
+values	(1, "one"),
+		(2, "two"),
+        (3, "three"),
+        (NULL, "noname1"),
+        (NULL, "noname2"),
+        (NULL, NULL),
+        (4, "four"),
+        (5, "five");
+        
+insert into t2
+values	(1, "one_2"),
+		(2, "two_2"),
+        (3, "three_2"),
+        (NULL, "noname1_2");
+
+
+desc t1;
+desc t2;
+
+select * from t1;
+select * from t2;
+-- -------------------------------------------------------------
+
+-- in <퀴즈 2-1>
+select	*
+from 	t1
+where 	id in(
+	select 	id
+    from	t2);
+/*
+t1의 id가 t2의 id 안에 있는지 확인
+속성들이 1:1로 비교됨
+t1의 table이 출력됨
+*** null은 출력되지 않음 ***
+joint와는 다르게 t2는 출력되지 않음
+*/
+
+-- not in <퀴즈2-2>
+select	*
+from	t1
+where	id	not in(
+	select	id
+    from	t2);
+/*
+1. 1,2,3은 t2에 있으므로 출력되지 않음
+2. 4,5는 t2 table에 null과 비교되어 출력되지 않음 (t2에 null이 없으면 4,5가 출력됨)
+null이 한쪽에라도 있으면 비교되는 경우 unkouwn이 출력되어 false --> 출력x
+*/
+
+-- not in <퀴즈2-3>
+select	*
+from	t1
+where	id	not in(
+	select	id
+    from	t2
+    where id is not null);
+/*
+null을 항상 고려해야..
+*** not in 철칙 : not in 뒤에 "is not null" 있어야
+*/
+
+-- exists <퀴즈2-4>
+select	*
+from	t1
+where	exists (
+	select	*
+    from	t2);
+-- t1이 몽땅 출력
+
+-- exists <퀴즈2-5>
+select	*
+from	t1
+where	exists (
+	select	*
+    from	t2
+    where	t1.id = t2.id);
+/*
+반드시 조건 포함
+where 이 없으면 t1이 몽땅 출력됨
+한쪽 table만 가져오므로 semi join
+*/
+
+-- not exists <퀴즈2-6>
+select	*
+from	t1
+where	not exists (
+	select	*
+    from	t2
+    where	t1.id = t2.id);
+    
+/*
+1,2,3 -> t2에 존재하므로 출력되지 않음
+4,5 -> t2에 존재하지 않으므로 출력됨
+null -> 한쪽이 null이면 출력
+*/
+
+
 -- index 삭제
 create index t1_id_index on t1(id);
 
@@ -12,3 +114,7 @@ create table t3(id int, addr varchar(20), index t3_id_index(id));
 create table t3(id int, addr varchar(20), key t3_id_index(id));
 
 desc t3;
+-- -------------------------------------------------------
+
+
+
---
+++ db_w9_web_ref/src/database/sql.js
@@ -0,0 +1,60 @@
+import mysql from 'mysql2';
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    port: 3306,
+    user:'root',
+    password:'',
+    database:'week9',
+});
+
+const promisePool = pool.promise();
+
+export const selectSql = {
+    getEmployee: async () => {
+        const sql = `select * from employee`;
+        const [result] = await promisePool.query(sql);
+
+        return result;
+    },
+
+    getDepartment: async () => {
+        const sql = `select * from department`;
+        const [result] = await promisePool.query(sql);
+
+        return result
+    }
+}
+
+export const insertSql = {
+    setEmployee: async (data) => {
+        const sql = `insert into employee values (
+            "${data.Fname}", "${data.Minit}", "${data.Lname}", "${data.Ssn}", "${data.Bdate}",
+            "${data.Address}", "${data.Sex}", "${data.Salary}", "${data.Super_ssn}", "${data.Dno}"
+        )`
+
+        await promisePool.query(sql);
+    },
+
+    setDepartment: async (data) => {
+        const sql = `insert into department values (
+            "${data.Dname}", "${data.Dnumber}", "${data.Mgr_ssn}", "${data.Mgr_start_date}"
+        )`
+
+        await promisePool.query(sql);
+    }
+};
+
+export const updateSql = {
+    updateEmployee: async () => {
+        const sql = `update employee set salary = 500 where Minit = "F"`;
+        await promisePool.query(sql);
+    },
+
+    updateDepartment: async (data) => {
+        console.log(data);
+        const sql = `update department set dname = "${data.Dname}" where Dnumber=5`;
+        console.log(sql);
+        await promisePool.query(sql);
+    }
+}
